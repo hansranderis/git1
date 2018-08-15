569e5854db6625f7dca0835137f7f88bdf530650
hans <hrn@eal.dk>
2018-08-15 11:11:21 +0200

    linie2

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,7 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git");
-            Console.WriteLine("heh he");
+            Console.WriteLine("linie2");
             Console.WriteLine("linie3");
             Console.ReadLine();
         }
